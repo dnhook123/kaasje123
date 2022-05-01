53613a43aacf12e993c633d23193d54cc484b75e
Weng,David D <d.weng@student.fontys.nl>
2022-05-01 13:29:21 +0200

    gdfsgfdsgdsf

--- index.js
+++
@@ -1,8 +0,0 @@
-import * as aws from "@pulumi/aws";
-
-// Create an AWS resource (S3 Bucket)
-function davidwengpang() {
-    const bucket = new aws.s3.Bucket("my-bucket");
-}
-
-module.exports = davidwengpang();
