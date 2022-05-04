b4331827403ca621a808d7b22126863d04b5e1a8
Weng,David D <d.weng@student.fontys.nl>
2022-05-04 10:10:24 +0200

    lol

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
 import * as aws from "@pulumi/aws";
+import * as pulumi from "@pulumi"
+const stackConfig = new pulumi.Config('backend-api-aws');
 
 // Create an AWS resource (S3 Bucket)
 function davidwengpang() {
-    const bucket = new aws.s3.Bucket("my-bucket");
+    const bucket = new aws.s3.Bucket(stackConfig.require('purposeName'),);
 }
 
 module.exports = davidwengpang();
